56a7c2107cb9cfdc124f29c52784cb40106984e2
hyya123 <hya741147@gmail.com>
2025-04-19 01:02:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,63 +9,84 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const question = '喜歡什麼汽車？日系 或 美系';
-let waitingQueue = [];
+// 多人排隊佇列
+const waitingUsers = [];
+const questions = [
+  '喜歡什麼汽車？日系 或 美系',
+  '喜歡哪種飲料？可樂 或 果汁',
+  '喜歡貓還是狗？貓 或 狗'
+];
 
 io.on('connection', (socket) => {
   console.log('使用者連線:', socket.id);
   socket.partner = null;
   socket.nickname = '';
-  socket.answer = null;
+  socket.answers = [];
 
   socket.on('start_pairing', ({ nickname }) => {
     socket.nickname = nickname;
+    socket.answers = [];
     socket.partner = null;
-    socket.answer = null;
 
-    if (waitingQueue.length > 0) {
-      const partner = waitingQueue.shift();
-
-      socket.partner = partner;
-      partner.partner = socket;
-
-      socket.emit('paired');
-      partner.emit('paired');
-
-      socket.emit('ask_question', question);
-      partner.emit('ask_question', question);
-    } else {
-      waitingQueue.push(socket);
-      socket.emit('waiting');
-    }
+    // 放入佇列並檢查配對
+    waitingUsers.push(socket);
+    tryPairing();
   });
 
-  socket.on('answer_question', (answer) => {
-    socket.answer = answer;
-    const partner = socket.partner;
+  function tryPairing() {
+    if (waitingUsers.length < 2) return;
 
-    if (partner && partner.answer !== null) {
-      if (partner.answer === socket.answer) {
-        socket.emit('question_matched', { partnerNickname: partner.nickname });
-        partner.emit('question_matched', { partnerNickname: socket.nickname });
+    const user1 = waitingUsers.shift();
+    const user2 = waitingUsers.shift();
+
+    user1.partner = user2;
+    user2.partner = user1;
+
+    user1.emit('paired', questions);
+    user2.emit('paired', questions);
+  }
+
+  socket.on('answer_question', (answers) => {
+    socket.answers = answers;
+
+    const partner = socket.partner;
+    if (partner && partner.answers.length === questions.length) {
+      // 雙方都回答完畢，進行比較
+      const matched = answers.filter((ans, i) => ans === partner.answers[i]).length;
+
+      if (matched >= 2) {
+        socket.emit('question_matched', partner.answers);
+        partner.emit('question_matched', socket.answers);
       } else {
         socket.emit('question_failed');
         partner.emit('question_failed');
 
+        // 回到等待配對
         socket.partner = null;
         partner.partner = null;
-        socket.answer = null;
-        partner.answer = null;
+        socket.answers = [];
+        partner.answers = [];
+        waitingUsers.push(socket, partner);
       }
     }
   });
 
-  socket.on('message', (text) => {
+  socket.on('message', (msg) => {
     if (socket.partner) {
       socket.partner.emit('message', {
         from: socket.nickname || '匿名',
-        text
+        text: msg
       });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.partner) {
+      socket.partner.emit('partner_left');
+      socket.partner.partner = null;
+    } else {
+      const index = waitingUsers.indexOf(socket);
+      if (index !== -1) waitingUsers.splice(index, 1);
     }
   });
 
@@ -74,22 +95,8 @@ io.on('connection', (socket) => {
       socket.partner.emit('partner_left');
       socket.partner.partner = null;
     }
-
     socket.partner = null;
-    socket.answer = null;
-
-    const idx = waitingQueue.indexOf(socket);
-    if (idx !== -1) waitingQueue.splice(idx, 1);
-  });
-
-  socket.on('disconnect', () => {
-    if (socket.partner) {
-      socket.partner.emit('partner_left');
-      socket.partner.partner = null;
-    }
-
-    const idx = waitingQueue.indexOf(socket);
-    if (idx !== -1) waitingQueue.splice(idx, 1);
+    socket.answers = [];
   });
 });
 
